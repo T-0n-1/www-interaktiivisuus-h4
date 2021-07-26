c472213f25b4158f3b8e1094c167b823b004e222
Toni Mertanen <t.mertanen@hotmail.com>
2021-07-27 00:55:53 +0300

    Code rewritten to use Firebase

---
+++ js/firebase.js
@@ -0,0 +1,13 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyCK-oEkblmjffu2EwNhCgg9RaDe_M7vuo0",
+    authDomain: "d3-firebase-b6302.firebaseapp.com",
+    databaseURL: "https://d3-firebase-b6302.firebaseio.com",
+    projectId: "d3-firebase-b6302",
+    storageBucket: "d3-firebase-b6302.appspot.com",
+    messagingSenderId: "909602705698",
+    appId: "1:909602705698:web:82797a8d555d5260"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore()
--- js/script.js
+++ js/script.js
@@ -19,47 +19,54 @@ const graph = svg.append("g")
 const xAxisGroup = graph.append("g")
 const yAxisGroup = graph.append("g")
 
-d3.json("js/dogsinfi.json").then(
-    data => {
-        const scaleY = d3.scaleLinear()
-            .domain([0, d3.max(data, d => d.count)])
-            .range([graphHeight, 0])
+//d3.json("js/dogsinfi.json").then(
+db.collection("dogsinfi").get().then(response => {
+    var data = []
 
-        const scaleX = d3.scaleBand()
-            .domain(data.map(item => item.breed))
-            .range([0, graphWidth])
-            .paddingInner(0.1)
+    response.docs.forEach(doc => {
+        data.push(doc.data())
+    })
 
-        const rects = graph.selectAll("rect")
-            .data(data)
-            .attr("width", scaleX.bandwidth)
-            .attr("height", d => graphHeight - scaleY(d.count))
-            .attr("fill", "red")
-            .attr("x", d => scaleX(d.breed))
-            .attr("y", d => scaleY(d.count))
-        
-        rects.enter()
-            .append("rect")
-            .attr("width", scaleX.bandwidth)
-            .attr("height", d => graphHeight - scaleY(d.count))
-            .attr("fill", "red")
-            .attr("x", d => scaleX(d.breed))
-            .attr("y", d => scaleY(d.count))
+//    data => {
+const scaleY = d3.scaleLinear()
+    .domain([0, d3.max(data, d => d.count)])
+    .range([graphHeight, 0])
 
-        const x_axis = d3.axisBottom()
-            .scale(scaleX)
-        
-        const y_axis = d3.axisLeft()
-            .scale(scaleY)
-            .ticks(5)
-            .tickFormat(d => d + " yksilöä")
-        
-        xAxisGroup.call(x_axis)
-            .attr("transform", `translate(0, ${graphHeight})`)
-        yAxisGroup.call(y_axis)
+const scaleX = d3.scaleBand()
+    .domain(data.map(item => item.breed))
+    .range([0, graphWidth])
+    .paddingInner(0.1)
 
-        xAxisGroup.selectAll("text")
-        .attr("transform", `translate(0, 10) rotate(90)`)
-        .attr("text-anchor", `start`)
-    }
-)
+const rects = graph.selectAll("rect")
+    .data(data)
+    .attr("width", scaleX.bandwidth)
+    .attr("height", d => graphHeight - scaleY(d.count))
+    .attr("fill", "red")
+    .attr("x", d => scaleX(d.breed))
+    .attr("y", d => scaleY(d.count))
+
+rects.enter()
+    .append("rect")
+    .attr("width", scaleX.bandwidth)
+    .attr("height", d => graphHeight - scaleY(d.count))
+    .attr("fill", "red")
+    .attr("x", d => scaleX(d.breed))
+    .attr("y", d => scaleY(d.count))
+
+const x_axis = d3.axisBottom()
+    .scale(scaleX)
+
+const y_axis = d3.axisLeft()
+    .scale(scaleY)
+    .ticks(5)
+    .tickFormat(d => d + " yksilöä")
+
+xAxisGroup.call(x_axis)
+    .attr("transform", `translate(0, ${graphHeight})`)
+yAxisGroup.call(y_axis)
+
+xAxisGroup.selectAll("text")
+    .attr("transform", `translate(0, 10) rotate(90)`)
+    .attr("text-anchor", `start`)
+
+})
